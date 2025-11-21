74768cc3735940354ec62c93424ff8112e11f695
Ningyc <caonorah.ca@gmail.com>
2025-11-21 17:57:36 -0500

    feature: implement aggregated access statistics for short links - p2

--- project/src/main/java/com/negoffer/shortlink/project/dao/mapper/ShortLinkMapper.java
+++ project/src/main/java/com/negoffer/shortlink/project/dao/mapper/ShortLinkMapper.java
@@ -2,9 +2,23 @@ package com.negoffer.shortlink.project.dao.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.negoffer.shortlink.project.dao.entity.ShortLinkDO;
+import org.apache.ibatis.annotations.Update;
+import org.apache.ibatis.annotations.Param;
 
 /**
  * Short link persistence layer
  */
 public interface ShortLinkMapper extends BaseMapper<ShortLinkDO> {
+    /**
+     * Increment short link access statistics
+     */
+    @Update("UPDATE t_link SET total_pv = total_pv + #{totalPv}, total_uv = total_uv + #{totalUv}, total_uip = total_uip + #{totalUip} " +
+            "WHERE gid = #{gid} AND full_short_url = #{fullShortUrl}")
+    void incrementStats(
+            @Param("gid") String gid,
+            @Param("fullShortUrl") String fullShortUrl,
+            @Param("totalPv") Integer totalPv,
+            @Param("totalUv") Integer totalUv,
+            @Param("totalUip") Integer totalUip
+    );
 }
