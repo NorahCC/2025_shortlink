828051ed2ef872ba3329a984d322e92a5ff98f01
Ningyc <caonorah.ca@gmail.com>
2025-11-22 18:32:04 -0500

    feature: implement today's statistics for short link monitoring

--- project/src/main/java/com/negoffer/shortlink/project/dao/entity/LinkStatsTodayDO.java
+++ project/src/main/java/com/negoffer/shortlink/project/dao/entity/LinkStatsTodayDO.java
@@ -2,7 +2,11 @@ package com.negoffer.shortlink.project.dao.entity;
 
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.negoffer.shortlink.project.common.database.BaseDO;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.Date;
 
 /**
@@ -10,6 +14,9 @@ import java.util.Date;
  */
 @TableName("t_link_stats_today")
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class LinkStatsTodayDO extends BaseDO {
 
     /**
@@ -45,5 +52,5 @@ public class LinkStatsTodayDO extends BaseDO {
     /**
      * Today's IP Count (Unique IPs)
      */
-    private Integer todayIpCount;
+    private Integer todayUip;
 }
--- project/src/main/java/com/negoffer/shortlink/project/dao/mapper/LinkStatsTodayMapper.java
+++ project/src/main/java/com/negoffer/shortlink/project/dao/mapper/LinkStatsTodayMapper.java
@@ -2,9 +2,19 @@ package com.negoffer.shortlink.project.dao.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.negoffer.shortlink.project.dao.entity.LinkStatsTodayDO;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
 
 /**
  * Short Link Today's Statistics Mapper
  */
 public interface LinkStatsTodayMapper extends BaseMapper<LinkStatsTodayDO> {
+
+    /**
+     * Records today's statistics monitoring data.
+     */
+    @Insert("INSERT INTO t_link_stats_today (full_short_url, gid, date,  today_uv, today_pv, today_uip, create_time, update_time, del_flag) " +
+            "VALUES( #{linkTodayStats.fullShortUrl}, #{linkTodayStats.gid}, #{linkTodayStats.date}, #{linkTodayStats.todayUv}, #{linkTodayStats.todayPv}, #{linkTodayStats.todayUip}, NOW(), NOW(), 0) " +
+            "ON DUPLICATE KEY UPDATE today_uv = today_uv +  #{linkTodayStats.todayUv}, today_pv = today_pv +  #{linkTodayStats.todayPv}, today_uip = today_uip +  #{linkTodayStats.todayUip};")
+    void shortLinkTodayState(@Param("linkTodayStats") LinkStatsTodayDO linkStatsTodayDO);
 }
